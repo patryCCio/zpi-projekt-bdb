253475019d44ca58199e29f318d8201222d40f62
Patryk Szczerbinski <patryk.szczerbinski@o2.pl>
2024-06-23 23:45:05 +0200

    Add login functionality

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zaloguj</title>
+    <title>Zaloguj się</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
